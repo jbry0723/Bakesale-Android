77f3ca73bbd05083c9e6a499501f0c35d27a2621
Justin Justin <jbry0723@gmail.com>
2021-07-04 10:39:27 -0400

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>Bakesale</Text>
+      </View>
+    );
+  }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  header:{
+    fontSize:40,
+  }
 });
+
+export default App;
